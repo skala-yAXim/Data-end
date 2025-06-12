8d01a36ae640e2a617361152550b6d4af58877df
Kim Se Eun <rlatpdms1014@gmail.com>
2025-06-12 09:30:19 +0900

    feat: 테스트용 엔드포인트 추가

--- app/api/endpoints.py
+++ app/api/endpoints.py
@@ -7,7 +7,8 @@ from app.pipeline.github_pipeline import save_github_data
 from app.pipeline.teams_post_pipeline import save_teams_posts_data
 from app.rdb.repository import find_all_teams, find_all_users, find_all_team_members, find_all_git_info
 from app.rdb.client import get_db
-from fastapi import APIRouter, Request, Depends
+from app.common.statics_report import save_user_activities_to_rdb
+from fastapi import APIRouter, Request, Depends, Path
 from sqlalchemy.orm import Session
 from typing import List
 
@@ -85,3 +86,15 @@ def get_all_team_members(db: Session = Depends(get_db)):
     모든 팀 멤버 정보를 반환합니다.
     """
     return find_all_git_info(db)
+
+@router.get("/vectordb/userActivity/{target_date}")
+def get_vector_user_activity(
+    target_date: str = Path(..., description="기준 날짜 (YYYY-MM-DD 형식) / 가급적 금요일로 테스트바랍니다.."),
+    db: Session = Depends(get_db)
+):
+    """
+    모든 팀 멤버의 활동 정보를 벡터DB에 저장합니다.
+    """
+    # TODO: 일요일 저녁에 배치 돌린다는 가정 하에, 7일 전으로 설정되게 할 것
+    # return save_user_activities_to_rdb("2025-05-30", db)
+    return save_user_activities_to_rdb(target_date, db)
