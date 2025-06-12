b697c370482b91f1cb062852abaef4a7c64429c6
Kim Se Eun <rlatpdms1014@gmail.com>
2025-06-12 09:26:24 +0900

    feat: 일별 사용자의 업무 통계 정보 저장 및 Team 테이블 연관관계 오류 수정

--- app/rdb/schema.py
+++ app/rdb/schema.py
@@ -1,6 +1,18 @@
-from sqlalchemy import Column, String, Integer, Boolean, ForeignKey, CheckConstraint, TIMESTAMP, DateTime
+from sqlalchemy import Column, String, Integer, Boolean, ForeignKey, CheckConstraint, TIMESTAMP, DateTime, Date
 from sqlalchemy.orm import relationship
 from app.rdb.client import Base
+from sqlalchemy import Enum as SQLAlchemyEnum
+import enum
+
+class Weekday(enum.IntEnum):
+    FRIDAY = 0
+    SATURDAY = 1
+    SUNDAY = 2
+    MONDAY = 3
+    TUESDAY = 4
+    WEDNESDAY = 5
+    THURSDAY = 6
+
 
 class Team(Base):
     __tablename__ = "team"
@@ -11,8 +23,6 @@ class Team(Base):
     installation_id = Column(String)
     created_at = Column(TIMESTAMP)
     updated_at = Column(TIMESTAMP)
-
-    members = relationship("TeamMember", back_populates="team")
 
 
 class User(Base):
@@ -39,13 +49,14 @@ class TeamMember(Base):
     role = Column(String)
     team_id = Column(String, ForeignKey("team.id"))
 
-    team = relationship("Team", back_populates="members")
+    team = relationship("Team", lazy="select")
 
     __table_args__ = (
         CheckConstraint(
             "role IN ('MEMBER', 'LEADER', 'USER', 'ADMIN')", name="team_member_role_check"
         ),
     )
+
 
 class GitInfo(Base):
     __tablename__ = "git_info"
@@ -62,4 +73,25 @@ class GitInfo(Base):
     avatar_url = Column(String)
 
     # 관계 설정 (양방향 관계를 원할 경우 users 모델에도 relationship 추가 필요)
+    user = relationship("User", lazy="select")
+
+
+class DailyUserActivity(Base):
+    __tablename__ = "daily_user_activity"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    report_date = Column(Date, nullable=False)
+    day = Column(SQLAlchemyEnum(Weekday, native_enum=False), nullable=False)
+    teams_post = Column(Integer, nullable=False)
+    email_send = Column(Integer, nullable=False)
+    email_receive = Column(Integer, nullable=False)
+    docs_docx = Column(Integer, nullable=False)
+    docs_xlsx = Column(Integer, nullable=False)
+    docs_txt = Column(Integer, nullable=False)
+    docs_etc = Column(Integer, nullable=False)
+    git_pull_request = Column(Integer, nullable=False)
+    git_commit = Column(Integer, nullable=False)
+    git_issue = Column(Integer, nullable=False)
+
     user = relationship("User", lazy="select")
