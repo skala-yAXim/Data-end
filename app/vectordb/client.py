94dd2caa41396f5fe2a48de30f7cc1eeb4528d10
jjminsuh <minsuh3203@gmail.com>
2025-06-25 10:39:20 +0900

    fix: flush 누락된 collection 이름 추가

--- app/vectordb/client.py
+++ app/vectordb/client.py
@@ -3,7 +3,7 @@ from qdrant_client import QdrantClient
 from qdrant_client.http.models import Distance, VectorParams
 import requests
 
-from app.common.config import DOCS_COLLECTION_NAME, EMAIL_COLLECTION_NAME, GIT_COLLECTION_NAME, TEAMS_COLLECTION_NAME
+from app.common.config import DOCS_COLLECTION_NAME, EMAIL_COLLECTION_NAME, GIT_COLLECTION_NAME, README_COLLECTION_NAME, TEAMS_COLLECTION_NAME
 
 def get_qdrant_client() -> QdrantClient:
   return QdrantClient(
@@ -20,7 +20,7 @@ def create_collection(client: QdrantClient, collection_name: str):
 def flush_all_collections():
     client = get_qdrant_client()
     # 모든 컬렉션 이름 가져오기
-    collections = [TEAMS_COLLECTION_NAME, EMAIL_COLLECTION_NAME, GIT_COLLECTION_NAME, DOCS_COLLECTION_NAME]
+    collections = [TEAMS_COLLECTION_NAME, EMAIL_COLLECTION_NAME, GIT_COLLECTION_NAME, DOCS_COLLECTION_NAME, README_COLLECTION_NAME]
 
     deleted = []
     errors = []
